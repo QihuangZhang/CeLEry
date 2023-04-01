5477f9cbcd7d4cee2b7610aeb9cf9a1e948e9e4a
Qihuang Zhang <qihuang.zh@gmail.com>
2023-04-01 12:37:18 -0400

    Update preprocess.py

--- code_paper/1_LIBD/preprocess.py
+++ code_paper/1_LIBD/preprocess.py
@@ -94,40 +94,61 @@ genelistuni = list( dict.fromkeys(genelist) )   # remove duplicates
 
 def Preprocess_SpTrans (studyID):
     """
-    Preprocess the spatial transcriptomic raw data and package into Annnoted Dataset
-    Parameters
-        -----------
-        studyID : string. the study ID of the LIBD datasets
-    Returns
-    -----------
-        No returns. File automatically generated.
+    Preprocess the spatial transcriptomic raw data and obtain the optimal DE genes
+    Parameters:
+        studyID (string): the study ID of the LIBD datasets
+    Returns:
+        gene_topDE_list (list): the list of gene set that contains the highest DE genes between layers
     """
-    adata = read_10x_h5("../data/LIBD/{studyID}/{studyID}_raw_feature_bc_matrix.h5".format(studyID = studyID))
-    spatial = pd.read_csv("../data/LIBD/{studyID}/tissue_positions_list.txt".format(studyID = studyID),sep=",", header = None, na_filter = False, index_col = 0) 
+    # Read in the spatial transcriptomic data from a 10x Genomics-formatted HDF5 file
+    adata = sc.read_10x_h5(f"../data/LIBD/{studyID}/{studyID}_raw_feature_bc_matrix.h5")
+    
+    # Read in the spatial coordinates of the tissue samples from a CSV file
+    spatial = pd.read_csv(f"../data/LIBD/{studyID}/tissue_positions_list.txt", sep=",", header=None, na_filter=False, index_col=0) 
+    
+    # Add the spatial coordinates to the data object
     adata.obs["x1"] = spatial[1]
     adata.obs["x2"] = spatial[2]
     adata.obs["x3"] = spatial[3]
-    # Select captured samples
+    
+    # Select only the samples that were captured
     adata = adata[adata.obs["x1"] == 1]
+    
+    # Ensure that all gene names are uppercase
     adata.var_names = [i.upper() for i in list(adata.var_names)]
+    
+    # Add the gene names as a column in the `var` attribute of the `adata` object
     adata.var["genename"] = adata.var.index.astype("str")
-    #
+    
+    # Remove the "x1" column from the `obs` attribute of the `adata` object
     del adata.obs["x1"]
-    #
+    
+    # Add a "Layer" column to the `obs` attribute of the `adata` object and initialize it to 0
     adata.obs["Layer"] = 0
-    LayerName =["L1","L2","L3","L4","L5","L6","WM"]
+    
+    # Define a list of layer names
+    LayerName =["L1", "L2", "L3", "L4", "L5", "L6", "WM"]
+    
+    # Loop through each layer and add the corresponding layer number to the `Layer` column of the `adata` object
+    # Also add the layer name as a separate column for convenience
     for i in range(7):
-        Layer = pd.read_csv("../data/LIBD/{studyID}/{studyID}_{Lname}_barcodes.txt".format(studyID = studyID, Lname = LayerName[i]), sep=",", header = None, na_filter = False, index_col = 0)
+        Layer = pd.read_csv(f"../data/LIBD/{studyID}/{studyID}_{LayerName[i]}_barcodes.txt", sep=",", header=None, na_filter=False, index_col=0)
         adata.obs.loc[Layer.index, "Layer"] = int(i+1)
         adata.obs.loc[Layer.index, "Layer_character"] = LayerName[i]
-    #  Remove the spots without any layer label      
-    adata=adata[adata.obs["Layer"]!=0]    # Newly added on May 25
-    #
-    #  Preprocessing
+    
+    # Remove the spots without any layer label
+    data = adata[adata.obs["Layer"] != 0]
+    
+    # Make the gene names unique
     adata.var_names_make_unique()
-    cel.prefilter_genes(adata,min_cells=3) # avoiding all genes are zeros
+    
+    # Filter out genes that are expressed in fewer than 3 cells
+    cel.prefilter_genes(adata, min_cells=3)
+    
+    # Filter out special genes (e.g. mitochondrial genes)
     cel.prefilter_specialgenes(adata)
-    #Normalize and take log for UMI-------
+    
+    # Normalize the data by cell and take the log of the UMI counts
     sc.pp.normalize_per_cell(adata)
     sc.pp.log1p(adata)
     #
@@ -136,10 +157,6 @@ def Preprocess_SpTrans (studyID):
     #
     #  Filter the Genes that are selected by SpaGCN
     genename = adata.var['genename']
-    # Get the gene list from the pre-screening
-    # genelistlist = [d_g[i] for i in  range(len(d_g))]  # transform dictionary to a list of lists
-    # genelist = sum(genelistlist, [])  # merge the list of lists
-    # genelistuni = list( dict.fromkeys(genelist) )   # remove duplicates
     genelistindex = [genename[genename == i].index[0] for i in genelistuni if  len(genename[genename == i])>0]  # only keep the genes that exists in SpT data
     # Filter the genelist and output the results
     bdata = adata[:,genelistindex]
